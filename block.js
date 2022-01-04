af8265e0844012cce2a8e874d8b8145ebcc527fc
Niko Febrianur <nikofebrianur@gmail.com>
2022-01-04 20:54:25 +0800

    mined block

--- block.js
+++ block.js
@@ -1,16 +1,24 @@
-const { GENESIS_DATA } = require('./config')
+const { GENESIS_DATA } = require('./config');
 
 class Block {
     constructor({ timestamp, lastHash, hash, data }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
-        this.data = data
+        this.data = data;
     }
 
     static genesis() {
-        return new Block(GENESIS_DATA);
+        return new this(GENESIS_DATA);
     };
+
+    static mineBlock({ lastBlock, data }) {
+        return new this({
+            timestamp: Date.now(),
+            lastHash: lastBlock.hash,
+            data
+        });
+    }
 };
 
 // const block1 = new Block({
