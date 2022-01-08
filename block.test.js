237bb1b153feb0d2978a6a1e99a3c54c73014689
Niko Febrianur <nikofebrianur@gmail.com>
2022-01-08 09:58:08 +0800

    add difficulty

--- block.test.js
+++ block.test.js
@@ -3,17 +3,21 @@ const { GENESIS_DATA } = require('./config')
 const cryptoHash = require('./crypto-hash')
 
 describe('Block', () => {
-    const timestamp = 'a-data'
-    const lastHash = 'a-lastHash'
-    const hash = 'a-hash'
-    const data = ['blockchain', 'data']
-    const block = new Block({ timestamp, lastHash, hash, data})
+    const timestamp = 'a-data';
+    const lastHash = 'a-lastHash';
+    const hash = 'a-hash';
+    const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
+    const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty})
     
     it('has timestamp, lastHash, hash, and data property', () => {
-        expect(block.timestamp).toEqual(timestamp)
-        expect(block.lastHash).toEqual(lastHash)
-        expect(block.hash).toEqual(hash)
-        expect(block.data).toEqual(data)
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 
     describe('genesis()', () => {
@@ -51,7 +55,19 @@ describe('Block', () => {
 
         it('creates a SHA-256 `hash` based on the proper inputs', () => {
             expect(minedBlock.hash)
-                .toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+                .toEqual(
+                    cryptoHash(
+                        minedBlock.timestamp, 
+                        minedBlock.nonce,
+                        minedBlock.difficulty, 
+                        lastBlock.hash,
+                        data
+                    )
+                );
         });
+
+        it('sets a `hash` that matched difficulty criteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual('0',repeat(minedBlock.difficulty));
+        })
     });
 });
